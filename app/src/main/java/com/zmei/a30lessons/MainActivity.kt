7c8496b0b31a22a6f7b966b311b9043e50c86c42
arhizmei <arhizmei@gmail.com>
2023-04-25 19:27:24 +0300

    work2

--- app/src/main/java/com/zmei/a30lessons/MainActivity.kt
+++ app/src/main/java/com/zmei/a30lessons/MainActivity.kt
@@ -2,10 +2,13 @@ package com.zmei.a30lessons
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.zmei.a30lessons.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
     override fun onCreate(savedInstanceState: Bundle?) {
+        binding= ActivityMainBinding.inflate(layoutInflater)
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(binding.root)
     }
 }
